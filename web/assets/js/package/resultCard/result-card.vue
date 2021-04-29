992a6105b92673aa78e1b7380674b54f63f4b52f
Peter Foerster <peter@foerster.tech>
2021-04-29 08:37:28 -0700

    result-page and result-card initialized
    both components can get loaded and be filled with data. Rudimentary data display is done.

---
+++ web/assets/js/package/resultCard/result-card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <p>id: {{ page.id }} - package name: {{ page.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: [ 'page' ]
+}
+</script>
+
+<style>
+div {
+    margin: 20px;
+    border: 1px dashed rebeccapurple;
+}
+</style>
--- web/assets/js/package/resultPage/result-page.vue
+++ web/assets/js/package/resultPage/result-page.vue
@@ -1,12 +1,35 @@
 <template>
-  <h1>This is the actual Vue</h1>
+  <div>
+    <h1>This is the actual Vue</h1>
+    <div v-for="card in packageList" :key="card.id">
+      <resultCard :page="card"></resultCard>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
+import resultCard from '../resultCard/result-card.vue'
+
+
+export default  {
     id: "ResultPage",
+    components: {
+      resultCard
+    },
+    data() {
+      return {
+        packageList: null
+      }
+    },
     mounted() {
       console.log('mounted')
+      axios.post('/api/packages/list').then((response) => {
+        console.log(response)
+        this.packageList = response.data.packages
+      }, (error) => {
+        console.log(error)
+      })
     }
 }
 </script>
